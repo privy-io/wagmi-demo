429f67a8822a3220b3ae68b0dec5aee91d8a63a2
Nicholas Charriere <nicholascharriere@gmail.com>
2023-01-12 14:57:47 -0800

    Initial commit + PRIVY integration working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {PrivyProvider} from '@privy-io/react-auth';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}>
+    <Component {...pageProps} />
+      </PrivyProvider>
+  )
 }
