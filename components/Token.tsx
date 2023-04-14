f103496804265ed6436786218b3df951e7d7c6fb
Payton Garland <payton.r.g@gmail.com>
2023-04-14 11:52:19 -0400

    fix typo

--- components/Token.tsx
+++ components/Token.tsx
@@ -20,7 +20,7 @@ const Token = () => {
       contractAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"; // USDC Mainnet
       break;
     case 5:
-      contractAddress = "0x2f3a40a3db8a7e3d09b0adfefbce4f6f81927557"; // USDC Goerlie (unofficial)
+      contractAddress = "0x2f3a40a3db8a7e3d09b0adfefbce4f6f81927557"; // USDC Goerli (unofficial)
       break;
     default:
       return (
