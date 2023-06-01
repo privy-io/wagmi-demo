2aac725c93028a4187ef7c6fb61833464c7b5085
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2023-06-01 16:56:20 -0400

    Bump packages for connect-only (#3)
    Also: Relax image loading constraints

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
     };
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
