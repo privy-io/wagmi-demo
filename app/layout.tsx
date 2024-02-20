ac2cf650067c49ba9eee078056c66e09ccf1cc4d
Ankush Swarnakar <ankushswar1@gmail.com>
2024-02-20 13:11:33 -0500

    fix providers import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 
-import Providers from 'components/Providers';
+import Providers from 'components/providers';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
